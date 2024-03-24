3cca77c9631ddd396291318b6e2ace1f0890e48d
JesserHamdaoui <107654958+JesserHamdaoui@users.noreply.github.com>
2024-03-24 06:05:43 +0100

    fix(token): hide inference token in dotenv

--- Backend/topic_classification.py
+++ Backend/topic_classification.py
@@ -1,7 +1,13 @@
 import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+inference_token = os.getenv("INFERENCE_TOKEN")
 
 API_URL = "https://api-inference.huggingface.co/models/dstefa/roberta-base_topic_classification_nyt_news"
-headers = {"Authorization": "Bearer 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"}
+headers = {"Authorization": "Bearer {inference_token}"}
 
 def query(payload):
 	response = requests.post(API_URL, headers=headers, json=payload)
